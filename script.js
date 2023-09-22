923d07dfea316cf2f77eb60f620f1e0210546895
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-09-23 01:50:43 +0530

    adding switching player logic

--- script.js
+++ script.js
@@ -8,14 +8,18 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0Element = document.getElementById('current--0');
 const current1Element = document.getElementById('current--1');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+
 
 let currentScore = 0;
+let activePlayer = 0;
 // Starting Conditions
 //Setting the Score to 0
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden'); //Hiding the dice initially
-
+  
 //Rolling dice Logic 
 btnRoll.addEventListener('click', function() {
     //Generating A random dice roll
@@ -29,7 +33,14 @@ btnRoll.addEventListener('click', function() {
     //Check for rolled If Rolled 1 Switch the Player
     if (dice !== 1) {
         // Add the score from the dice
-        currentScore += dice;
-        current0Element.textContent = currentScore;
-    } else {}
+        currentScore += dice; 
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        activePlayer = (activePlayer === 0) ? 1 : 0;
+        //toggle checks if the class is there or not if its present it will remove else it will add. 
+        player0Element.classList.toggle('player--active'); 
+        player1Element.classList.toggle('player--active'); 
+    }
 });
